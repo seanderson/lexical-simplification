ddf46450a79ff4ea0ca95e54dbfbfa384fcb94bb
NLP <hefischel@gmail.com>
2018-06-20 11:53:57 -0400

    featureClassification work

--- src/featureClassification.py
+++ src/featureClassification.py
@@ -10,19 +10,23 @@ import classpaths as paths
 from nltk.corpus import wordnet
 
 
-def cwictorify(corpus):
+def cwictorify(inputPath, outputPath):
     # format: Sentence   word    indexInSent     BinaryIsComplex
-    input = open(corpus)
-    output = open()
-    for line in input:
-        list = line.split('\t')
-        print(line)
+    with open(inputPath) as file:
+        input = file.readlines()
+    with open(outputPath,"w") as output:
+        for line in input:
+            list = line.split('\t')
+            #print(list)
+            if list[2] > 3:
+                c = 1
+            else:
+                c = 0
+            output.write(list[3]+"\t"+list[0]+"\t"+list[1]+"\t"+str(c)+"\n")
+    return outputPath
 
-    return output
 
-
-
-def main(corpus):
+def main(corpus, output):
     m = MorphAdornerToolkit(paths.MORPH_ADORNER_TOOLKIT)
 
     fe = FeatureEstimator()
@@ -32,10 +36,10 @@ def main(corpus):
     # word freq (from google n-gram)
     # unique WordNet synsets
     # WordNet synonyms
-    return fe.calculateFeatures(cwictorify(corpus), format='cwictor')
+    return fe.calculateFeatures(cwictorify(corpus,output), format='cwictor')
 
 
 if __name__ == '__main__':
     # main('train_cwictor_corpus.txt', 'test_cwictor_corpus.txt')
     print(main(paths.NEWSELA_COMPLEX +
-               "Newsela_Complex_Words_Dataset_supplied.txt"))
+               "Newsela_Complex_Words_Dataset_supplied.txt", paths.NEWSELA_COMPLEX+"Cwictorified"))
